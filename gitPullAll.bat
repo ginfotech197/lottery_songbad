dfc12ebf9da82f453bfaab728b42f22283315451
priyamghosh1 <priyamghosh73@gmail.com>
2023-02-27 20:41:43 +0530

    updated draw master

---
+++ gitPullAll.bat
@@ -0,0 +1,10 @@
+cd lottery_songbad_angular
+git pull
+
+cd..
+
+cd lottery_songbad_api
+git pull
+
+cd ..
+git pull
---
+++ gitPush.bat
@@ -0,0 +1,18 @@
+cd lottery_songbad_angular
+git add .
+git commit -m %1
+git pull
+git push
+
+cd..
+cd lottery_songbad_api
+git add .
+git commit -m %1
+git pull
+git push
+
+cd..
+git add .
+git commit -m %1
+git pull
+git push
